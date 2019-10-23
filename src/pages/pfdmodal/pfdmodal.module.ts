dc83ea06f2c3b00ea5ce9c65f0aa3a894df8c28f
shahrulhaniff <shahrul941013@gmail.com>
2019-10-23 12:47:52 +0800

    buang dekat pdfmodalmodule ts

--- src/pages/pfdmodal/pfdmodal.module.ts
+++ src/pages/pfdmodal/pfdmodal.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { PfdmodalPage } from './pfdmodal';
 
-import { DocumentViewer } from '@ionic-native/document-viewer';
-import { PdfViewerModule } from 'ng2-pdf-viewer';
 
 @NgModule({
   declarations: [
@@ -11,8 +9,6 @@ import { PdfViewerModule } from 'ng2-pdf-viewer';
   ],
   imports: [
     IonicPageModule.forChild(PfdmodalPage),
-    ,DocumentViewer
-    ,PdfViewerModule
   ],
 })
 export class PfdmodalPageModule {}
